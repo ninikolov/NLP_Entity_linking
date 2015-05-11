86288a92a9a6a344df35e0a1d1f23e2177761676
Wolf Vollprecht <w.vollprecht@gmail.com>
2015-05-11 16:00:09 +0200

    blabla wikimatch nicer

--- wikimatch.py
+++ wikimatch.py
@@ -15,9 +15,6 @@ psql = psycopg2.connect("dbname=mw user=wolfv")
 cur = psql.cursor()
 
 
-def get_most_probable_disambiguation(q):
-    entities = q.entities
-
 def wiki_cached_get(ent):
     print(ent)
     if r.exists(ent):
@@ -37,6 +34,7 @@ def wiki_cached_get(ent):
 
 def match_disambiguation(disam_entity, other):
 
+    ds = []
     r = wiki_cached_get(disam_entity)
     if type(r) == DisambiguationError:
         "All is going according to planb"
@@ -69,7 +67,6 @@ def select_best_entity(entity_collection, other):
     for e_str in entity_collection:
         possible_entities.append(wiki_cached_get(e_str))
 
-
     current_res = None
     current_max_prob = 0
     for m in possible_entities:
@@ -87,14 +84,15 @@ def select_best_entity(entity_collection, other):
 
             c = other_entity.content
             c = re.sub('[^0-9a-zA-Z\s]+', '', c)
+
             cur.execute("select ts_rank(to_tsvector(%s), to_tsquery(%s))", (m.content, " | ".join(c.split())))
             res = cur.fetchone()
             if res[0] > current_max_prob:
                 current_res = m
                 curr_max_prob = res[0]
+
     return current_res.title.replace(' ', '_')
 
-            print("FULLFULLTEXTMATCH: " , res)
 
 def main():
     queries = [{
@@ -103,40 +101,9 @@ def main():
     }]
 
     ds = []
-    #for e in queries[0]['entities']:
-    #    r = wiki_cached_get(e)
-    #    if type(r) == DisambiguationError:
-    #       print(r.options)
-    #        disambiguation = str(r)
-    #        for d in r.options:
-    #            wk_d = wiki_cached_get(d)
-    #            if type(wk_d) != DisambiguationError:
-    #                ds.append(wk_d)
-    #e_match = wiki_cached_get('Online_banking')
 
-    select_best_entity(['Green_laser', 'Viridian Green Laser Sights', 'Sights'], 'Glock')
+    match_disambiguation('PNC', 'online banking')
+    select_best_entity(['Green_laser', 'Viridian Green Laser Sights'], 'Glock')
 
-    # curr_max_prob = 0
-    # for d in ds:
-    #     cur.execute("select ts_rank(to_tsvector(%s), to_tsquery(%s))", (d.summary, " | ".join('online banking'.split())))
-    #     res = cur.fetchone()
-    #     if res[0] > curr_max_prob:
-    #         curr_max_prob = res[0]
-    #         curr_new_ent = d.title
-
-    #     print(d.title, res)
-    # #print("The selected entity is: ", curr_new_ent)
-
-    # m_set_1 = wiki_cached_get('Glock')
-    # m_set_2 = [
-    #     wiki_cached_get('Green_laser'),
-    #     wiki_cached_get('Viridian Green Laser Sights')
-    # ]
-
-    # for m in m_set_2:
-    #     print(m.content)
-    #     cur.execute("select ts_rank(to_tsvector(%s), to_tsquery(%s))", (m.content, " | ".join('glock'.split())))
-    #     res = cur.fetchone()
-    #     print(m.title, res[0])
 if __name__ == "__main__":
     main()
